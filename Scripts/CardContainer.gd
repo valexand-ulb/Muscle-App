82e27c043cd21d8ee452a2a5ae4d9e3d4ca55d03
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2022-10-25 21:50:25 +0200

    resize

---
+++ Scripts/CardContainer.gd
@@ -0,0 +1,24 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_New_pressed():
+	var CardScene = load("res://Scenes/Card.tscn")
+	var _Card_instance = CardScene.instance()
+	add_child(_Card_instance)
+	$"../../../../../.."._disable_scrollbars()
+
--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -1,15 +1,10 @@
 extends Control
 
 
-func _disable_scrollbar():
+func _disable_scrollbars():
 	$MainScrollBox.get_h_scrollbar().rect_scale.x = 0
+	$MainScrollBox/HBoxContainer/ColorRectWhite/HBoxContainer/ScrollContainer.get_h_scrollbar().rect_scale.x = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_disable_scrollbar()
-	
-func _on_New_pressed():
-	var CardScene = load("res://Scenes/Card.tscn")
-	var _Card_instance = CardScene.instance()
-	
-	$MainScrollBox/HBoxContainer/ColorRectWhite/HBoxContainer/ScrollContainer/VBoxContainer.add_child(_Card_instance)
+	_disable_scrollbars()
