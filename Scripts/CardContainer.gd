04a4b699b6433cdcecdce9e467bcb15865a20529
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2022-10-31 17:31:29 +0100

    Changes on the layout ; less containers, anchors changes

--- Scripts/CardContainer.gd
+++ Scripts/CardContainer.gd
@@ -20,5 +20,5 @@ func _on_New_pressed():
 	var CardScene = load("res://Scenes/Card.tscn")
 	var _Card_instance = CardScene.instance()
 	add_child(_Card_instance)
-	$"../../../../../.."._disable_scrollbars()
+	$"../../../../.."._disable_scrollbars()
 
--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _disable_scrollbars():
 	$MainScrollBox.get_h_scrollbar().rect_scale.x = 0
-	$MainScrollBox/HBoxContainer/ColorRectWhite/HBoxContainer/ScrollContainer.get_h_scrollbar().rect_scale.x = 0
+	$MainScrollBox/HBoxContainer/ColorRectWhite/ScrollContainer.get_h_scrollbar().rect_scale.x = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
