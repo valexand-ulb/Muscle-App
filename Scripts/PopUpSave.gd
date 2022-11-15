746fd28365296d3767509ae2f9c229a8b359c46e
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2022-11-15 10:38:47 +0100

    popup changes

--- Scripts/PopUpSave.gd
+++ Scripts/PopUpSave.gd
@@ -1,6 +1,7 @@
 extends WindowDialog
 
-
+onready var save_button = get_node("MarginContainer/VBoxContainer/Button")
+onready var line_edit = get_node("MarginContainer/VBoxContainer/Button")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,8 +10,12 @@ extends WindowDialog
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_close_button().visible = 0
+	save_button.visible = 0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+func _on_LineEdit_text_entered(new_text):
+	save_button.visible = 1
+	if ! new_text :
+		save_button.visible = 0
